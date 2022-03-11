58fd3ffc5e71dacc9703105564898a434c1b0d48
Cezer <chriseliezer@hotmail.com>
2022-03-11 15:58:18 +0100

    feat: :sparkles: wrapped up frame picker and render

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { ArtworkPicker, ColorPicker, Navbar, RenderContainer, Shirt } from "@components";
+import { ArtworkPicker, ColorPicker, FramePicker, Navbar, RenderContainer, Shirt } from "@components";
 import useTranslation from "next-translate/useTranslation";
 import React from "react";
 
@@ -10,7 +10,7 @@ const Home: React.FC<{ defaultNamespace: string }> = ({ defaultNamespace }) => {
       <Navbar t={t} />
       <section className="grid grid-cols-1 md:grid-cols-[1fr_0.65fr] main__content">
         {/* Left Section */}
-        <aside className="px-4 md:px-20 flex h-full max-h-full border-r border-r-slate-200">
+        <aside className="px-4 md:px-20 flex h-full min-h-full border-r border-r-slate-200">
           <div className="flex flex-col my-auto">
             <ColorPicker />
           </div>
@@ -20,9 +20,9 @@ const Home: React.FC<{ defaultNamespace: string }> = ({ defaultNamespace }) => {
           </div>
         </aside>
         {/* Right Section */}
-        <aside className="px-6 md:p-20 max-h-full overflow-y-auto">
+        <aside className="px-6 md:p-20 min-h-full overflow-y-auto">
           <ArtworkPicker t={t} />
-          <h2>{t`home:select`}</h2>
+          <FramePicker t={t} />
           <h2>{t`home:size`}</h2>
           <p className="text-gray-200">{t`home:details`}</p>
         </aside>
